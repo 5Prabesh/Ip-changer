c4e5742573f7accd7ea036122e2a77af9193e80d
Prabesh Bakhrel <83901025+5Prabesh@users.noreply.github.com>
2022-01-24 14:13:00 +0545

    Update install.py

--- install.py
+++ install.py
@@ -3,16 +3,17 @@ choice = input('[+] to install press (Y) to uninstall press (N) >> ')
 run = os.system
 if str(choice) =='Y' or str(choice)=='y':
 
-    run('chmod 777 IP-changer.py')
+    run('chmod 777 ipchanger.py')
     run('mkdir /usr/share/aut')
-    run('cp autoTOR.py /usr/share/aut/IP-changer.py')
+    run('cp autoTOR.py /usr/share/aut/ipchanger.py')
 
-    cmnd=(' #! /bin/sh \n exec python3 /usr/share/aut/IP-changer.py "$@"')
+    cmnd=(' #! /bin/sh \n exec python3 /usr/share/aut/ipchanger.py "$@"')
     with open('/usr/bin/aut','w')as file:
         file.write(cmnd)
-    run('chmod +x /usr/bin/aut & chmod +x /usr/share/aut/IP-changer.py')
+    run('chmod +x /usr/bin/aut & chmod +x /usr/share/aut/ipchanger.py')
     print('''\n\ncongratulation Ip Changer is installed successfully \nfrom now just type \x1b[6;30;42maut\x1b[0m in terminal ''')
 if str(choice)=='N' or str(choice)=='n':
     run('rm -r /usr/share/aut ')
     run('rm /usr/bin/aut ')
     print('[!] now Auto Tor Ip changer  has been removed successfully')
+
